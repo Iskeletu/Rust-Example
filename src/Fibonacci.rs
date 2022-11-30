abec623fa7dbdb42efe6c896bb4ff863949155c6
Fábio Gandini <80495916+Iskeletu@users.noreply.github.com>
2022-11-29 23:15:23 -0300

    Update Fibonacci.rs

--- src/Fibonacci.rs
+++ src/Fibonacci.rs
@@ -1,1 +1,21 @@
+// Fábio Gandini - 2022
+// Simple rust Fibonacci sequence implementation.
 
+//Prints fibonacci sequence given a sequence size as a limiter.
+fn fibonacci(sequence_size: i32) {
+    if sequence_size > 0 {
+        let mut x = 0;
+        let mut y = 1;
+        
+        for _value in 0..sequence_size {
+            println!("{}", x);
+            x = x + y;
+            y = x - y;
+        }
+    }
+}
+
+
+fn main() {
+    fibonacci(10);
+}
